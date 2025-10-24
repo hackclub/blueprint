362a04f20b8a4a414dc50af9d414a1a9d95e3777
Sam Liu <sam@samliu.dev>
2025-10-24 10:28:13 -0400

    Make view counter more performant

--- app/models/project.rb
+++ app/models/project.rb
@@ -472,18 +472,12 @@ class Project < ApplicationRecord
   end
 
   def view_count
-    Ahoy::Event.where(name: "project_view")
-      .where("properties @> ?", { project_id: id }.to_json)
-      .count("DISTINCT ((properties->>'user_id')::bigint)")
+    views_count
   end
 
   def self.view_counts_for(project_ids)
     return {} if project_ids.blank?
-    Ahoy::Event.where(name: "project_view")
-      .where("properties->>'project_id' IN (?)", project_ids.map(&:to_s))
-      .group("properties->>'project_id'")
-      .count("DISTINCT ((properties->>'user_id')::bigint)")
-      .transform_keys(&:to_i)
+    Project.where(id: project_ids).pluck(:id, :views_count).to_h
   end
 
   def self.follower_counts_for(project_ids)
