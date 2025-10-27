03851d2032d24d687f44b16c2fe97607f4069c5d
Sam Liu <sam@samliu.dev>
2025-10-27 11:01:37 -0400

    Lint

--- db/migrate/20251024155431_add_indexes_to_journal_entries.rb
+++ db/migrate/20251024155431_add_indexes_to_journal_entries.rb
@@ -2,7 +2,7 @@ class AddIndexesToJournalEntries < ActiveRecord::Migration[8.0]
   def change
     # Index for polymorphic review association lookups
     add_index :journal_entries, [ :review_type, :review_id ]
-    
+
     # Partial index for finding unreviewed entries by project
     add_index :journal_entries, [ :project_id, :created_at ],
               where: "review_id IS NULL",
