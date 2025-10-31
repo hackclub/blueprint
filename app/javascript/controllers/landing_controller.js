7e04f05a793be82cc3bd170c161c38f722d7ee12
Sam Liu <sam@samliu.dev>
2025-10-31 12:09:09 -0400

    Add new landing section

--- app/javascript/controllers/landing_controller.js
+++ app/javascript/controllers/landing_controller.js
@@ -1,20 +1,19 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-
-  static targets = ["hero", "heroOutline", "heroContent", "overlay"]
+  static targets = ["hero", "heroOutline", "heroContent", "overlay"];
 
   connect() {
     this.heroImage = this.heroTarget.children[0];
 
-    this.heroImage.addEventListener("load", this.onLoad.bind(this))
-    document.addEventListener("scroll", this.onScroll.bind(this))
-    this.onScroll()
+    this.heroImage.addEventListener("load", this.onLoad.bind(this));
+    document.addEventListener("scroll", this.onScroll.bind(this));
+    this.onScroll();
   }
 
   disconnect() {
-    this.heroImage.removeEventListener("load", this.onLoad.bind(this))
-    document.removeEventListener("scroll", this.onScroll.bind(this))
+    this.heroImage.removeEventListener("load", this.onLoad.bind(this));
+    document.removeEventListener("scroll", this.onScroll.bind(this));
   }
 
   onLoad() {
@@ -22,13 +21,19 @@ export default class extends Controller {
   }
 
   onScroll() {
-    const scrollPercent = Math.max(0, Math.min(100, (window.scrollY / (this.heroHeight || window.innerHeight)) * 100))
+    const scrollPercent = Math.max(
+      0,
+      Math.min(
+        100,
+        (window.scrollY / (this.heroHeight || window.innerHeight)) * 100
+      )
+    );
 
-    let angle = 180 + (120 - 180) * scrollPercent / 100;
-    let percent = 100 - (30 + 70 * scrollPercent / 100);
+    let angle = 180 + ((120 - 180) * scrollPercent) / 100;
+    let percent = 100 - (30 + (70 * scrollPercent) / 100);
 
-    this.heroTarget.style.maskImage = `linear-gradient(${angle}deg, rgba(0,0,0,0.5) ${percent}%, rgba(0,0,0,0) ${percent}%)`
-    this.heroOutlineTarget.style.maskImage = `linear-gradient(${angle}deg, rgba(0,0,0,0) ${percent}%, rgba(0,0,0,1) ${percent}%)`
-    this.overlayTarget.style.maskImage = `linear-gradient(${angle}deg, rgba(0,0,0,1) ${percent}%, rgba(0,0,0,0) ${percent}%)`
+    this.heroTarget.style.maskImage = `linear-gradient(${angle}deg, rgba(0,0,0,0.4) ${percent}%, rgba(0,0,0,0) ${percent}%)`;
+    this.heroOutlineTarget.style.maskImage = `linear-gradient(${angle}deg, rgba(0,0,0,0) ${percent}%, rgba(0,0,0,1) ${percent}%)`;
+    this.overlayTarget.style.maskImage = `linear-gradient(${angle}deg, rgba(0,0,0,1) ${percent}%, rgba(0,0,0,0) ${percent}%)`;
   }
 }
