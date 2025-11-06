96cbbb27d13134cd7ae63a05836bad59f9373ff8
Clay Nicholson <82461712+claynicholson@users.noreply.github.com>
2025-11-06 11:01:44 -0500

    Fixed ts

--- app/controllers/prototype_controller.rb
+++ app/controllers/prototype_controller.rb
@@ -1,6 +1,7 @@
 class PrototypeController < ApplicationController
   http_basic_authenticate_with name: ENV["PROTOTYPE_USER"], password: ENV["PROTOTYPE_PASS"]
-  layout false
+  allow_unauthenticated_access only: %i[index]
+  layout "prototype"
 
   def index
   end
