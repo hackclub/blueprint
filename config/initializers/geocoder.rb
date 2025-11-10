0791f1e49a2c9c365973955edfddb6c33ce7a90a
Sam Liu <sam@samliu.dev>
2025-11-10 13:21:44 -0500

    Update geocoder for cloudflare

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,3 +1,10 @@
+if defined?(Geocoder::Request::GEOCODER_CANDIDATE_HEADERS)
+  Geocoder::Request::GEOCODER_CANDIDATE_HEADERS.unshift(
+    "HTTP_CF_CONNECTING_IP",
+    "HTTP_TRUE_CLIENT_IP"
+  )
+end
+
 Geocoder.configure(
   timeout: 2,
   ip_lookup: :hack_club,
