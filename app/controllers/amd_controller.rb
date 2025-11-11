21861c4459061bcce93830272d9158f19898eb0d
Sam Liu <sam@samliu.dev>
2025-11-10 22:19:45 -0500

    Better tracking

--- app/controllers/amd_controller.rb
+++ app/controllers/amd_controller.rb
@@ -1,5 +1,6 @@
 class AmdController < ApplicationController
   allow_unauthenticated_access only: %i[index]
+  skip_after_action :track_page_view, only: :index
 
   def index
     redirect_to root_path(utm_source: "amd"), allow_other_host: false
