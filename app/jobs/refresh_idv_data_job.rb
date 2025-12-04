e9963fd581808efae258c9b971773f0104149ee5
Sam Liu <sam@samliu.dev>
2025-12-04 15:29:12 -0500

    Don't make IDV job fail

--- app/jobs/refresh_idv_data_job.rb
+++ app/jobs/refresh_idv_data_job.rb
@@ -4,6 +4,9 @@ class RefreshIdvDataJob < ApplicationJob
   def perform
     User.find_each do |user|
       user.refresh_idv_data!
+    rescue Faraday::UnauthorizedError => e
+      Rails.logger.warn("IDV refresh failed for user #{user.id}: #{e.message}")
+      Sentry.capture_exception(e, extra: { user_id: user.id })
     end
   end
 end
