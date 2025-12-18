ef640e324a21b49b11e4977c1d5ed5574c21b03e
Sam Liu <sam@samliu.dev>
2025-12-17 23:43:48 -0800

    Update airtable sync logging

--- app/jobs/airtable_sync_job.rb
+++ app/jobs/airtable_sync_job.rb
@@ -4,11 +4,15 @@ class AirtableSyncJob < ApplicationJob
   def perform(*args)
     classes_to_sync = [ User.name, Project.name, ShopOrder.name ]
 
+    errors = []
+
     classes_to_sync.each do |classname|
-      Sentry.with_scope do |scope|
-        scope.set_tags(airtable_sync_class: classname)
-        AirtableSync.sync!(classname)
-      end
+      AirtableSync.sync!(classname)
+    rescue => e
+      Sentry.capture_exception(e, extra: { airtable_sync_class: classname })
+      errors << { classname: classname, error: e }
     end
+
+    Rails.logger.error("AirtableSync failed for: #{errors.map { |e| e[:classname] }.join(', ')}") if errors.any?
   end
 end
