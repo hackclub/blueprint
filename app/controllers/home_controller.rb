4ec47765c677a7efe4b0f4e6dfc09e2067b0758b
Sam Liu <sam@samliu.dev>
2026-01-25 11:56:34 -0500

    Remove unnecessary includes for banner_attachment in projects query

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @projects = current_user.projects.where(is_deleted: false).includes(:banner_attachment)
+    @projects = current_user.projects.where(is_deleted: false)
     @viral_projects = Project.where(viral: true, is_deleted: false)
                              .order_by_recent_journal
                              .limit(10)
