a2a292a3d1ad0625a0903b95b669ac01fd88dc16
Sam Liu <sam@samliu.dev>
2026-02-02 17:41:28 -0500

    Remove unused associations from projects query in HomeController

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @projects = current_user.projects.where(is_deleted: false).includes(:demo_picture_attachment, :latest_journal_entry, :followers, :kudos)
+    @projects = current_user.projects.where(is_deleted: false).includes(:demo_picture_attachment, :latest_journal_entry)
     @viral_projects = Project.where(viral: true, is_deleted: false)
                              .order_by_recent_journal
                              .limit(10)
