e0152c1a119bff9fc215db77e30a7add8d8e84c2
Sn3akySqu1d <elliottrpreston2007@gmail.com>
2026-03-11 04:22:42 +0000

    added guilds page

--- app/controllers/guilds_controller.rb
+++ app/controllers/guilds_controller.rb
@@ -1,4 +1,8 @@
 class GuildsController < ApplicationController
+  # currently anyone can hit this URL but the UI will hide/disable the link
+  # for users who aren't in expert/pro mode.  We still fetch the list of guilds
+  # so the view can render them when the feature ships.
   def index
+    @guilds = Guild.order(:name)
   end
 end
